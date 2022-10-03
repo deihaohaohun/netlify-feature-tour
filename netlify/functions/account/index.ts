51b686d4a36f2c395c00973db454feb86e7aac67
mengxl8 <mengxl8@lenovo.com>
2022-10-03 12:44:29 +0800

    测试cors

--- netlify/functions/account/index.ts
+++ netlify/functions/account/index.ts
@@ -2,19 +2,17 @@ import accounts from "./account.json";
 import { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event, context) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  };
+
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify(accounts),
   };
 };
 
 export { handler };
-
-module.exports.addRating = (event, context, callback) => {
-  callback(null, {
-    statusCode: 201,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
-};
