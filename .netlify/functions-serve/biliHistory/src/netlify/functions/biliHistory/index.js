28d294620a368483ff4f795295f4848107b3f35f
DESKTOP-7UBFFS4\28075 <youdou173339@163.com>
2022-10-23 12:29:51 +0800

    更新历史数据

--- .netlify/functions-serve/biliHistory/src/netlify/functions/biliHistory/index.js
+++ .netlify/functions-serve/biliHistory/src/netlify/functions/biliHistory/index.js
@@ -24,7 +24,57 @@ __export(biliHistory_exports, {
 module.exports = __toCommonJS(biliHistory_exports);
 
 // netlify/functions/biliHistory/biliHistory.json
+var doing = [
+  {
+    title: "\u7CBE\u7075\u5B9D\u53EF\u68A6 \u65E0\u5370",
+    cover: "https://i0.hdslb.com/bfs/bangumi/297063889e7836facea595b63eb6330f51904a24.jpg@220w_280h.webp"
+  },
+  {
+    title: "\u8857\u89D2\u9B54\u65CF \u7B2C\u4E8C\u5B63",
+    cover: "https://i0.hdslb.com/bfs/bangumi/image/a29f3b00a94d1b4a029054f72963cc10d98bae99.jpg@268w_358h.webp"
+  },
+  {
+    title: "\u56DB\u8272\u6218\u8BB0 / RWBY \u7B2C\u4E00\u5B63",
+    cover: "https://i0.hdslb.com/bfs/bangumi/image/21bfba22ad670f0b8162055198bcdd23780dcb0d.png@220w_280h.webp"
+  },
+  {
+    title: "\u5723\u8BDE\u4E4B\u543BSS",
+    cover: "https://i0.hdslb.com/bfs/bangumi/0cf19817077713b638513b1636e4d86989bff9c7.jpg@220w_280h.webp"
+  },
+  {
+    title: "\u590F\u65E5\u91CD\u73B0",
+    cover: "https://i0.hdslb.com/bfs/bangumi/image/4b2547ffdd6f40f997454b37c0ad48624a1730d9.jpg@220w_280h.webp"
+  },
+  {
+    title: "\u9F50\u6728\u6960\u96C4\u7684\u707E\u96BE\uFF08\u65E5\u64AD&\u7CBE\u9009\u7248\uFF09",
+    cover: "https://i0.hdslb.com/bfs/bangumi/eb4f17335f48951945fb9da47e6ee0bc65fa2fbb.jpg@220w_280h.webp"
+  },
+  {
+    title: "\u8DEF\u4EBA\u8D85\u80FD100 III(\u7075\u80FD\u767E\u5206\u767E \u7B2C\u4E09\u5B63)",
+    cover: "https://i0.hdslb.com/bfs/bangumi/image/f298e43fd9673abdaf86ecb30ba8197410ee3c3e.png@220w_280h.webp"
+  },
+  {
+    title: "\u98DF\u8349\u8001\u9F99\u88AB\u51A0\u4EE5\u6076\u9F99\u4E4B\u540D",
+    cover: "https://i0.hdslb.com/bfs/bangumi/image/303b4b9b3c244cd56e2c8315b985e9600714c13c.png@220w_280h.webp"
+  },
+  {
+    title: "\u56FD\u738B\u6392\u540D",
+    cover: "https://i0.hdslb.com/bfs/bangumi/image/8aead7650fb3ded88f24654b7b66823169cb9a8c.png@220w_280h.webp"
+  },
+  {
+    title: "\u732B\u548C\u8001\u9F20\u559C\u5267\u79C0\uFF081980\uFF09",
+    cover: "https://i0.hdslb.com/bfs/bangumi/image/d716a784aad6c1d5778f82004036520520bed474.jpg@220w_280h.webp"
+  },
+  {
+    title: "\u53F2\u52AA\u6BD4\u592A\u7A7A\u5386\u9669\u8BB0 \u7B2C\u4E00\u5B63",
+    cover: "https://i0.hdslb.com/bfs/bangumi/image/31202ecba405d3682b701348c7de75f9b414739e.png@220w_280h.webp"
+  }
+];
 var done = [
+  {
+    title: "\u6253\u5DE5\u5427\uFF01\u9B54\u738B\u5927\u4EBA \u7B2C\u4E8C\u5B63",
+    cover: "https://i0.hdslb.com/bfs/bangumi/image/282fdd17a5efdc226b0e0f0f077ce13d132f8646.png@268w_358h.webp"
+  },
   {
     title: "\uFF24\uFF52\uFF0E\uFF33\uFF34\uFF2F\uFF2E\uFF25 \u77F3\u7EAA\u5143 \u7279\u522B\u7BC7\uFF1A\u9F99\u6C34",
     cover: "https://i0.hdslb.com/bfs/bangumi/image/81a4c708722395b6be78a3412ecfd7dd90a71aef.png@220w_280h.webp"
@@ -32,10 +82,6 @@ var done = [
   {
     title: "\u8857\u89D2\u9B54\u65CF \u7B2C\u4E00\u5B63",
     cover: "https://i0.hdslb.com/bfs/bangumi/8fa97d1f68517724a0afcb1ca536bfe5b818e0d8.png@220w_280h.webp"
-  },
-  {
-    title: "\u8857\u89D2\u9B54\u65CF \u7B2C\u4E8C\u5B63",
-    cover: "https://i0.hdslb.com/bfs/bangumi/image/a29f3b00a94d1b4a029054f72963cc10d98bae99.jpg@268w_358h.webp"
   },
   {
     title: "\u6708\u5149\u4E0B\u7684\u5F02\u4E16\u754C\u4E4B\u65C5",
@@ -74,48 +120,6 @@ var done = [
     cover: "https://i0.hdslb.com/bfs/bangumi/image/b348bb6f42d6a9603130ab15a9dccef980ba1a19.png@220w_280h.webp"
   }
 ];
-var doing = [
-  {
-    title: "\u56DB\u8272\u6218\u8BB0 / RWBY \u7B2C\u4E00\u5B63",
-    cover: "https://i0.hdslb.com/bfs/bangumi/image/21bfba22ad670f0b8162055198bcdd23780dcb0d.png@220w_280h.webp"
-  },
-  {
-    title: "\u5723\u8BDE\u4E4B\u543BSS",
-    cover: "https://i0.hdslb.com/bfs/bangumi/0cf19817077713b638513b1636e4d86989bff9c7.jpg@220w_280h.webp"
-  },
-  {
-    title: "\u590F\u65E5\u91CD\u73B0",
-    cover: "https://i0.hdslb.com/bfs/bangumi/image/4b2547ffdd6f40f997454b37c0ad48624a1730d9.jpg@220w_280h.webp"
-  },
-  {
-    title: "\u9F50\u6728\u6960\u96C4\u7684\u707E\u96BE\uFF08\u65E5\u64AD&\u7CBE\u9009\u7248\uFF09",
-    cover: "https://i0.hdslb.com/bfs/bangumi/eb4f17335f48951945fb9da47e6ee0bc65fa2fbb.jpg@220w_280h.webp"
-  },
-  {
-    title: "\u8DEF\u4EBA\u8D85\u80FD100 III(\u7075\u80FD\u767E\u5206\u767E \u7B2C\u4E09\u5B63)",
-    cover: "https://i0.hdslb.com/bfs/bangumi/image/f298e43fd9673abdaf86ecb30ba8197410ee3c3e.png@220w_280h.webp"
-  },
-  {
-    title: "\u98DF\u8349\u8001\u9F99\u88AB\u51A0\u4EE5\u6076\u9F99\u4E4B\u540D",
-    cover: "https://i0.hdslb.com/bfs/bangumi/image/303b4b9b3c244cd56e2c8315b985e9600714c13c.png@220w_280h.webp"
-  },
-  {
-    title: "\u6253\u5DE5\u5427\uFF01\u9B54\u738B\u5927\u4EBA \u7B2C\u4E8C\u5B63",
-    cover: "https://i0.hdslb.com/bfs/bangumi/image/282fdd17a5efdc226b0e0f0f077ce13d132f8646.png@268w_358h.webp"
-  },
-  {
-    title: "\u56FD\u738B\u6392\u540D",
-    cover: "https://i0.hdslb.com/bfs/bangumi/image/8aead7650fb3ded88f24654b7b66823169cb9a8c.png@220w_280h.webp"
-  },
-  {
-    title: "\u732B\u548C\u8001\u9F20\u559C\u5267\u79C0\uFF081980\uFF09",
-    cover: "https://i0.hdslb.com/bfs/bangumi/image/d716a784aad6c1d5778f82004036520520bed474.jpg@220w_280h.webp"
-  },
-  {
-    title: "\u53F2\u52AA\u6BD4\u592A\u7A7A\u5386\u9669\u8BB0 \u7B2C\u4E00\u5B63",
-    cover: "https://i0.hdslb.com/bfs/bangumi/image/31202ecba405d3682b701348c7de75f9b414739e.png@220w_280h.webp"
-  }
-];
 var todo = [
   {
     title: "\u672C\u7530\u5C0F\u72FC\u4E0E\u6211",
@@ -128,15 +132,11 @@ var todo = [
   {
     title: "\u5F0F\u5B88\u540C\u5B66\u4E0D\u53EA\u53EF\u7231\u800C\u5DF2",
     cover: "https://i0.hdslb.com/bfs/bangumi/image/9918f6e67c26d6bd1ddad8dab70bcc7af3bfae9b.png@220w_280h.webp"
-  },
-  {
-    title: "\u7CBE\u7075\u5B9D\u53EF\u68A6 \u65E0\u5370",
-    cover: "https://i0.hdslb.com/bfs/bangumi/297063889e7836facea595b63eb6330f51904a24.jpg@220w_280h.webp"
   }
 ];
 var biliHistory_default = {
-  done,
   doing,
+  done,
   todo
 };
 
